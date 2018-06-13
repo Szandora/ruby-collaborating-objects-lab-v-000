bb866648aa93ae1646203aa952d1c1a144dcf7c0
Sheila Wittman <szandora789@gmail.com>
2018-06-13 21:57:21 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def import
+    file.each do |file|
+      song = song.new_by_filename(file)
+    end
+  end
+
+  def files
+    @filenames = Dir.entries(@path).select{|f| File.file?(@path+"/"+f)}
+  end
+end
